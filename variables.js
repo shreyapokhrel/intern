455361278c951f1dfb198372920e69e65c923d98
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-05 10:30:30 +0545

    Update and rename README.md to variables.js

---
+++ variables.js
@@ -0,0 +1,22 @@
+let fullname = "shreya pokhrel";
+let age=22;
+X=null;
+y=undefined;
+isFollow=true;
+console.log(age);
+console.log(fullname);
+console.log(X);
+console.log(y);
+console.log(isFollow);
+const PI = 3.12;
+console.log(PI);
+{
+let a=13;
+console.log(a);
+}
+{
+let a=10;
+console.log(a);
+}
+
+
