b8a54e183d4636681160ab8a4608a431e6ab0931
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-05 12:07:42 +0545

    Create array.js

---
+++ array.js
@@ -0,0 +1,33 @@
+let marks = [93,54,64,53,54,];
+ console.log(marks);
+ console.log(marks.length);//property
+
+ let heroes = ["ironman" , "thor", "hulk"]
+ console.log(heroes);
+
+//loops(for)
+let heroes = ["ironman" , "thor", "hulk","spiderman","antman"]
+  
+ for(let idx=0; idx < heroes.length; idx++){
+    console.log(heroes[idx]);
+ }
+//for of
+ for (let el of heroes){
+    console.log(el);
+ }
+
+let cities =["delhi","pune" ,"mumbai"];
+  for (let city of cities){
+    console.log(city.toUpperCase());
+  }
+let marks = [55,33,66,55,99,44,33];
+   let sum = 0;
+//
+for(let val of marks){
+    sum += val;
+}
+
+let avg = sum / marks.length;
+console.log('avg marks of the class = ${avg}');
+
+
