5847b67ce025eb35b264ad5bd172179af3a21b96
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-06 12:05:41 +0545

    Update object and class.js

--- object and class.js
+++ object and class.js
@@ -20,6 +20,7 @@ const karanArjun ={
     salary : 50000,
 };
  karanArjun._proto_=employee;
+
 //class
 
 class ToyotaCar{
@@ -36,3 +37,27 @@ class ToyotaCar{
 
  let fortuner = new ToyotaCar();
  fortuner.setBrand("fortuner");
+
+//constructor
+
+class ToyotaCar{
+    constructor(brand,mileage){
+        console.log("creating new object");
+        this.brand =brand;
+        this.milage =mileage;
+    }
+    start(){
+       console.log("start");
+    }
+    stop(){
+        console.log("stop");
+    
+      }
+    
+ }
+
+ let fortuner = new ToyotaCar("fortuner",10);//constructor
+ console.log(fortuner);
+ let lexus = new ToyotaCar ("lexus",12) ;//constructor
+ console.log(lexus);
+ 
