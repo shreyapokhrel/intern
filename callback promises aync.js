9f6a86424b93e0ed6f284be86063f80ff9d0980b
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-06 16:59:59 +0545

    Update callback promises aync.js

--- callback promises aync.js
+++ callback promises aync.js
@@ -1,4 +1,4 @@
--> async
+-> asynchronous
 
 console.log("one");
 console.log("three");
@@ -23,3 +23,44 @@ function calculator(a,b,sumCallback) {
 calculator(1,2,(a,b) => {
     console.log(a+b);
 });
+
+//callback hell
+
+function getData(dataId , getNextdata){
+   setTimeout(()=> {
+        console.log("data", dataId);
+        if (getNextData){
+            getNextData();
+        }
+    }, 2000);
+ }
+getData(1,() => {
+    getData(2);
+});
+
+//promises
+/resolve 
+let promise = new Promise ((resolve,reject) => {
+    console.log("I am a promise");
+    resolve("success");
+});
+
+/reject
+
+let promise = new Promise ((resolve,reject) => {
+    console.log("I am a promise");
+    reject("some error");
+});
+//
+
+function getData(dataId, getNextData) {
+    return new Promise ((resolve,reject) => {
+    setTimeout(() => {
+        console.log("data", dataId);
+        resolve("success");
+        if (getNextData) {
+            getNextData();
+        }
+    }, 5000);
+});
+}
