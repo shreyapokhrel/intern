91650c5a01229a9365aef3aebda13574f76b05b7
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-06 17:34:53 +0545

    Update callback promises aync.js

--- callback promises aync.js
+++ callback promises aync.js
@@ -82,3 +82,19 @@ promise.catch(() =>{
     console.log("rejected");
 });
 
+//promise chaining
+function asyncFunc(){
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=> {
+            console.log("data1");
+            resolve("success");
+        }, 4000);
+    });
+}
+console.log("fetching data1..");
+let p1 = asyncFunc();
+p1.then((res) => {
+    console.log(res);
+});
+
+
