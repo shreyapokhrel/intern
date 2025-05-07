d529a11da62f7117f47bfcd110d11e519d5d1dc5
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-07 13:49:37 +0545

    Update typeconversion.js

--- typeconversion.js
+++ typeconversion.js
@@ -13,6 +13,19 @@ console.log(arr.length,(typeof arr));
 let i = 75;
 console.log (i.toString())
 
+//number
+let stri = Number("3456");
+stri = Number("23c4");
+stri = Number(false);
+console.log(stri,(typeof stri));
+
+//parse
+let number = parseInt('34.34') ;
+console.log(number,(typeof number));
+
+
+
+
 
 
 
