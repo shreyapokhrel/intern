a84bc3aedd0f0230169937afa46291543e0412af
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-07 16:58:02 +0545

    Update Destructuring.js

--- Destructuring.js
+++ Destructuring.js
@@ -1,1 +1,19 @@
+const colors = ['red', 'green', 'yellow', 'pink', 'black', 'white']
 
+// const color1 = colors[0]
+// const color2 = colors[1]
+// const color3 = colors[2]
+
+const [color1, color2, color3] = colors
+
+// const [,,,color4] = colors
+const { 3: color4, 5: color5 } = colors
+
+const user = {
+  name: 'Anurag',
+  age: 25,
+  address: {
+    city: 'Bangalore',
+    state: 'Karnataka',
+  },
+}
