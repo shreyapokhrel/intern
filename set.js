43cfb38477e2aed0d5e3d288343976bd8f878470
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-07 14:58:24 +0545

    Update set.js

--- set.js
+++ set.js
@@ -13,3 +13,23 @@ let mySet2 = new Set([1, 43, 'this',false,{a:1,b:3}]);
 console.log('New set:',mySet2);
 
 console.log(mySet.size)
+//has
+
+console.log(mySet.has(345));
+
+//delete
+
+console.log('Before removal',mySet.has('this'));
+mySet.delete('this')
+console.log('After removal',mySet.has('this'));
+
+//iterating
+
+for(let item of mySet){
+    console.log('items is:',item);
+}
+
+mySet.forEach((item) =>{
+    console.log('Item is :', item);
+})
+
