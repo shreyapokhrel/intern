4013155fb2ba4e51d4444ca9a2ded727b785ef48
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-07 12:16:18 +0545

    Update typeconversion.js

--- typeconversion.js
+++ typeconversion.js
@@ -1,1 +1,18 @@
+//type conversion
+let myVar;
+myVar = String(34);
+let booleanVar = String(true);
+console.log(booleanVar,(typeof booleanVar));
+
+let date = String(new Date());
+console.log(date,(typeof date));
+
+let arr = String([1,2,3,4,5]);
+console.log(arr.length,(typeof arr));
+
+let i = 75;
+console.log (i.toString())
+
+
+
 
