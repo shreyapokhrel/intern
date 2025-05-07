f8bd5b22409864472e47fa4bcfa4f9348eb07b62
shreya pokhrel <135811679+shreyapokhrel@users.noreply.github.com>
2025-05-07 14:39:09 +0545

    Update set.js

--- set.js
+++ set.js
@@ -1,1 +1,15 @@
+const mySet = new Set();
+console.log('The set looks like:', mySet);
 
+mySet.add('this');
+mySet.add('My name');
+mySet.add('that');
+mySet.add('34');
+mySet.add('true');
+mySet.add('false');
+console.log('The set looks like this now:',mySet);
+
+let mySet2 = new Set([1, 43, 'this',false,{a:1,b:3}]);
+console.log('New set:',mySet2);
+
+console.log(mySet.size)
