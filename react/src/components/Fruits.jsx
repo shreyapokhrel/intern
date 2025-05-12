e94dc705cc487a8536213738ba9bb678dd1242d7
shreya pokhrel <pokhrelshreya944@gmail.com>
2025-05-12 18:47:15 +0545

    add

--- react/src/components/Fruits.jsx
+++ react/src/components/Fruits.jsx
@@ -1,4 +1,4 @@
-export default function Fruits() {
+/* export default function Fruits() {
   const fruits = ["Apple", "Mango", "Orange"];
   return (
     <div>
@@ -6,6 +6,26 @@ export default function Fruits() {
         {fruits.map((fruit) => (
           <li key={fruit}>{fruit}</li>
           //<h1>{fruit}</h1>
+        ))}
+      </ul>
+    </div>
+  );
+} */
+export default function Fruits() {
+  const fruits = [
+    { name: "Apple", price: 10, emoji: "🍎" },
+    { name: "Mango", price: 20, emoji: "🥭" },
+    { name: "orange", price: 15, emoji: "🍊" },
+  ];
+  return (
+    <div>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.name}>
+            {fruit.emoji}
+            {fruit.name}
+            {fruit.price}
+          </li>
         ))}
       </ul>
     </div>
