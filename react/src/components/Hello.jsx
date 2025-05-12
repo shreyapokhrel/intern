1d4fda4d710dc5ab4db5f541d4e6600c08fb5220
shreya pokhrel <pokhrelshreya944@gmail.com>
2025-05-12 15:43:47 +0545

    add

--- react/src/components/Hello.jsx
+++ react/src/components/Hello.jsx
@@ -1,5 +1,20 @@
- function Hello(){
-    return <h1>Hello from a component!</h1>;
- }
+/*  const name = "Roe";
+function displayMessage() {
+  return "wow!";
+} 
+function Hello() {
+  return <h1>Hello from a component!{displayMessage()}</h1>;
+}
 
- export default Hello;
+export default Hello;
+ */
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello from a component! </h1> <h1>Wow!</h1>
+    </div>
+  );
+}
+
+export default Hello;
