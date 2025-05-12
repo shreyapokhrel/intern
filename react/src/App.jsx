e5d5a6da47d36d4b07107c48cf1ecbf8522c54d9
shreya pokhrel <pokhrelshreya944@gmail.com>
2025-05-12 13:56:51 +0545

    add

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
+/* import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css' */
 
+import Hello from "./components/Hello" ;
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+  return  <div className="App">
+    <Hello/>
+    
+
+  </div>;
+      /*  { <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  ) } */
 }
 
-export default App
+export default App;
